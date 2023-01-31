ce113e4bf56baa4267675e05e1eb081669a0d1ef
Tadj Cazaubon <tadj.d.cazaubon@gmail.com>
2023-01-30 22:11:20 -0800

    Delete tvhead directory

--- tvhead/utility/parsers.py
+++
@@ -1,7 +0,0 @@
-# parsing funtions to make argument passing to each script easier.
-def parseStringtoInt(integer:str) -> int:
-    try:
-        out = int(integer)
-    except ValueError:
-        return False
-    return out
