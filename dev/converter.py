49743d69b6e85ddf287746e1f4da0376bbf3ad0c
Tadj Cazaubon <Tadj.d.cazaubon@gmail.com>
2023-02-01 06:10:37 -0800

    _

--- dev/converter.py
+++ dev/converter.py
@@ -21,17 +21,19 @@ def get_res() -> tuple[int, int]:
 # Create a new numpy array in the form [LED index, r, g ,b] 
 def convert_image(path:str, write_folder: str, target_dimensions:tuple[int,int]) -> None:
     global header
+
+    #====================================================================================#
+
     # read in the png file
     img = cv2.imread(path, cv2.IMREAD_COLOR)
-    
     # Read the dimensions of the image.
     height, width, _ = img.shape
     # If the dimensions dont match the display dimensions, resize it.
-    
     if target_dimensions!=(width, height):
         # print(f"Image is {width} x {height}, we want {target_dimensions[0]} x {target_dimensions[1]}")
         img = cv2.resize(img, target_dimensions, interpolation = cv2.INTER_AREA)
 
+    #====================================================================================#
 
     filename = os.path.basename(path)[:-4]+".csv"
     # print(filename)
@@ -50,6 +52,7 @@ def convert_image(path:str, write_folder: str, target_dimensions:tuple[int,int])
     if not os.path.exists(f"{write_folder}/{foldername}"):
             os.mkdir(f"{write_folder}/{foldername}")
 
+    #====================================================================================#
     # flatten image to 2d array
     img_vector = img.reshape(-1, img.shape[-1])
 
@@ -60,6 +63,8 @@ def convert_image(path:str, write_folder: str, target_dimensions:tuple[int,int])
             pixels.append([i, img_vector[i][2], img_vector[i][1], img_vector[i][0]])
     #if len(pixels) == 0: pixels.append([0,0,0,0])
 
+    #====================================================================================#
+
     with open(savepath, "w+", encoding = "utf-8", newline = '') as f:
         writer = csv.writer(f)
         writer.writerow(header)
@@ -68,7 +73,6 @@ def convert_image(path:str, write_folder: str, target_dimensions:tuple[int,int])
 
 # Convert all images in a folder sequentially.
 def convert_frames(folder_path:str, write_folder:str, target_dimensions:tuple[int,int]) -> None:
-
     if os.path.isdir(folder_path):
         for root, _, images in os.walk(folder_path):
             for image in images:
@@ -97,7 +101,6 @@ def main() -> None:
     write_folder = 'upload/csvs'
 
     convert_all("dev/images", write_folder, (tw,th))
-    # convert_frames("dev/images/blink", write_folder)
 
 
 if __name__ == "__main__":
