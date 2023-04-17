0c145895409163d1768f66528b9222624b8beeef
Devinci <hervorarvitr@gmail.com>
2023-04-17 21:50:49 +0200

    attempting to get uart working between pico & esp

--- upload/pico/main.py
+++ upload/pico/main.py
@@ -4,8 +4,11 @@ The pi pico acts as a pseudo schmitt trigger for our analog values.
 The different steps/categories of change for each knob is on the esp32, the pico is concerned with
 signalling the esp32 when the value of a knob changes drastically, causing an interrupt on the esp32. 
 """
-from machine import Pin, ADC
+from machine import Pin, ADC, UART
 from time import sleep_ms
+import machine
+import ustruct
+
 
 br_pot = ADC(28)
 sp_pot = ADC(27)
@@ -15,19 +18,23 @@ br_pin = Pin(10, Pin.OUT)
 sp_pin = Pin(11, Pin.OUT)
 an_pin = Pin(12, Pin.OUT)
 
+TX = 4
+RX = 5
+# initialize UART with baudrate of 115200, 8 data bits, no parity, 1 stop bit, and pins 1 and 0 for TX and RX, respectively
+uart = UART(1, baudrate=115200, bits=8, parity=None, stop=1, tx=Pin(TX), rx=Pin(RX))
+
 
 values = {
-  "brightness" : 300,
-  "speed" : 300,
-  "animation" : 300
+  "Brightness" : 0,
+  "Speed" : 0,
+  "Channel" : 0
   }
 
 pins = [
-  ["brightness", br_pin],
-  ["speed", sp_pin],
-  ["animation", an_pin]
+  ["Brightness", br_pin],
+  ["Speed", sp_pin],
+  ["Channel", an_pin]
 ]
-
 
 def signal(key:str):
     global pins
@@ -35,25 +42,47 @@ def signal(key:str):
         if pin_details[0] == key:
             pin_details[1].value(1)
             sleep_ms(50)
-            pins[key].value(0)
+            pin_details[1].value(0)
             break
-    
+
+def send_over_UART(string_data, value):
+    # pack a tuple containing a string and a 16-bit number
+    data = (string_data, value)
+    packed_data = ustruct.pack("<16sH", data[0].encode(), data[1])
+
+    # send the packed data over UART
+    uart.write(packed_data)
+    print("\t> Sent: ",string_data,"\t",value)
+
 
 def compare(key:str, val2:int) -> None:
     global values
-    if val2*0.80 <= values[key] <= val2*0.80:
-        signal(key, val2)
-        values[key] = val2
+    if values[key]*0.75 <= val2 <= values[key]*1.25:
+        return
+    signal(key)
+    print(key + ": " + str(val2))
+    values[key] = val2
+    send_over_UART(key, val2)
+    
+def average_reading(pin):
+    readings = []
+    for i in range(30):
+        readings.append(pin.read_u16())
+        sleep_ms(5)
+    return int(sum(readings)/30)
       
 def monitor() -> None:
     global values
     global pins
     for pin in pins:
         pin[1].value(0)
-    for i in range(200):
-        compare("brightness", br_pot.read_u16())
-        compare("speed", sp_pot.read_u16())
-        compare("animation", an_pot.read_u16())
+    while True:
+        br_avg = average_reading(br_pot)
+        compare("Brightness", br_avg)
+        sp_avg = average_reading(sp_pot)
+        compare("Speed", sp_avg)
+        an_avg = average_reading(an_pot)
+        compare("Channel", an_avg)
         sleep_ms(300)
 
 
